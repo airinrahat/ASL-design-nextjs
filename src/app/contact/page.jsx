9c865decf52df481181e4220f200238e9a5c4644
Airin Rahat Prome <airinrahatprome@gmail.com>
2024-11-12 13:22:21 +0600

    some issue fixed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ContactBanner from "./ContactBanner";
 import OurAddress from "./OurAddress";
 import FAQ from "@/components/HomePage/FAQ";
+import CallUs from "@/components/HomePage/CallUs";
 
 const page = () => {
   return (
@@ -9,6 +10,7 @@ const page = () => {
       <ContactBanner />
       <OurAddress />
       <FAQ />
+      <CallUs />
     </div>
   );
 };
--- src/app/download/page.jsx
+++ src/app/download/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DownloadBanner from "./DownloadBanner";
 import FAQ from "@/components/HomePage/FAQ";
+import CallUs from "@/components/HomePage/CallUs";
 
 const page = () => {
   return (
     <div className="bg-white ">
       <DownloadBanner />
       <FAQ />
+      <CallUs />
     </div>
   );
 };
